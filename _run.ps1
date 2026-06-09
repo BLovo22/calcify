faf1415ac18f587b9d5b870e2154ac337aa3003f
BLovo22 <409439910@qq.com>
2026-06-09 11:57:05 +0800

    Clean up temp build scripts

--- _run.ps1
+++
@@ -1,4 +0,0 @@
-$base = "C:\Users\admin\Desktop\xiangmu\duihua"
-$html = Get-Content "$base\_sg.txt" -Raw
-[System.IO.File]::WriteAllText("$base\savings-goal-calculator.html", $html, [System.Text.UTF8Encoding]::new($false))
-Write-Output "Savings Goal written: $($html.Length) chars"
